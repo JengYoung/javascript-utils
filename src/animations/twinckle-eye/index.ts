4c2631f237bbab2dd85757d08938e298b6f6bceb
JengYoung <young_pallete@naver.com>
2022-09-27 15:45:52 +0900

    :sparkles: 초기에 눈 깜빡이는 애니메이션 구현
    requestAnimationFrame을 이용하여 애니메이션으로 구현

--- src/animations/twinckle-eye/index.ts
+++ src/animations/twinckle-eye/index.ts
@@ -1,80 +1,146 @@
 export class TwinkleEye {
+  isInitOpenEye: boolean;
+
+  isCloseEye: boolean;
+
   target: HTMLElement;
 
   sight: HTMLCanvasElement;
 
+  ctx: CanvasRenderingContext2D;
+
   height: number;
 
-  constructor(target: HTMLElement, height: number = 100) {
+  skewEyeDegree: number;
+
+  constructor(
+    target: HTMLElement,
+    height: number = 100,
+    skewEyeDegree: number = 0.04,
+  ) {
+    this.isInitOpenEye = false;
+    this.isCloseEye = false;
+
     this.target = target;
+    this.skewEyeDegree = skewEyeDegree;
+    this.height = height;
 
     this.sight = document.createElement('canvas');
     this.sight.classList.add('twinkle-eye-sight');
+    this.target.appendChild(this.sight);
 
-    this.sight.width = window.screen.width;
+    this.ctx = this.sight.getContext('2d') as CanvasRenderingContext2D;
+
+    this.sight.width = window.innerWidth;
     this.sight.height = window.innerHeight;
 
-    this.height = height;
+    window.addEventListener('resize', this.resize.bind(this));
 
-    const ctx = this.sight.getContext('2d') as CanvasRenderingContext2D;
+    requestAnimationFrame(this.animate.bind(this));
+  }
 
-    ctx.beginPath();
-    ctx.moveTo(0, 0);
-    ctx.lineTo(0, (this.sight.height - this.height) * 0.5);
+  animate() {
+    this.ctx.clearRect(0, 0, this.sight.width, this.sight.height);
 
-    ctx.bezierCurveTo(
+    this.init();
+
+    this.render();
+
+    requestAnimationFrame(this.animate.bind(this));
+  }
+
+  init() {
+    const initOpenEye = (height: number) => {
+      if (this.isInitOpenEye) return;
+
+      if (this.height >= height) {
+        this.isInitOpenEye = true;
+        return;
+      }
+
+      console.log(this.height);
+
+      this.height *= 1.1;
+    };
+
+    const closeEye = (height: number) => {
+      if (!this.isInitOpenEye || this.isCloseEye) return;
+
+      if (Math.floor(this.height) <= height) {
+        this.isCloseEye = true;
+        return;
+      }
+
+      this.skewEyeDegree *= 0.975;
+      this.height *= 0.95;
+    };
+
+    initOpenEye(200);
+    closeEye(0);
+  }
+
+  resize() {
+    this.sight.width = window.innerWidth;
+    this.sight.height = window.innerHeight;
+
+    this.render();
+  }
+
+  render() {
+    this.ctx.beginPath();
+    this.ctx.moveTo(0, 0);
+    this.ctx.lineTo(0, (this.sight.height - this.height) * 0.5);
+
+    this.ctx.bezierCurveTo(
       this.sight.width * 0.25,
-      this.sight.height * 0.4 - this.height * 0.5,
+      this.sight.height * (0.5 - this.skewEyeDegree) - this.height * 0.5,
       this.sight.width * 0.5,
-      this.sight.height * 0.4 - this.height * 0.5,
+      this.sight.height * (0.5 - this.skewEyeDegree) - this.height * 0.5,
       this.sight.width * 0.5,
-      this.sight.height * 0.4 - this.height * 0.5,
+      this.sight.height * (0.5 - this.skewEyeDegree) - this.height * 0.5,
     );
 
-    ctx.bezierCurveTo(
+    this.ctx.bezierCurveTo(
       this.sight.width * 0.5,
-      this.sight.height * 0.4 - this.height * 0.5,
+      this.sight.height * (0.5 - this.skewEyeDegree) - this.height * 0.5,
       this.sight.width * 0.75,
-      this.sight.height * 0.4 - this.height * 0.5,
+      this.sight.height * (0.5 - this.skewEyeDegree) - this.height * 0.5,
       this.sight.width,
       this.sight.height * 0.5 - this.height * 0.5,
     );
 
-    ctx.lineTo(this.sight.width, 0);
+    this.ctx.lineTo(this.sight.width, 0);
 
-    ctx.fillStyle = 'rgba(0, 0, 0)';
-    ctx.fill();
+    this.ctx.fillStyle = 'rgba(0, 0, 0)';
+    this.ctx.fill();
 
     // 아래쪽 눈도 구현한다.
-    ctx.beginPath();
-    ctx.moveTo(0, (this.sight.height + this.height) * 0.5);
-    ctx.bezierCurveTo(
+    this.ctx.beginPath();
+    this.ctx.moveTo(0, (this.sight.height + this.height) * 0.5);
+    this.ctx.bezierCurveTo(
       this.sight.width * 0.25,
-      this.sight.height * 0.6 + this.height * 0.5,
+      this.sight.height * (0.5 + this.skewEyeDegree) + this.height * 0.5,
       this.sight.width * 0.5,
-      this.sight.height * 0.6 + this.height * 0.5,
+      this.sight.height * (0.5 + this.skewEyeDegree) + this.height * 0.5,
       this.sight.width * 0.5,
-      this.sight.height * 0.6 + this.height * 0.5,
+      this.sight.height * (0.5 + this.skewEyeDegree) + this.height * 0.5,
     );
 
-    ctx.bezierCurveTo(
+    this.ctx.bezierCurveTo(
       this.sight.width * 0.5,
-      this.sight.height * 0.6 + this.height * 0.5,
+      this.sight.height * (0.5 + this.skewEyeDegree) + this.height * 0.5,
       this.sight.width * 0.75,
-      this.sight.height * 0.6 + this.height * 0.5,
+      this.sight.height * (0.5 + this.skewEyeDegree) + this.height * 0.5,
       this.sight.width,
       this.sight.height * 0.5 + this.height * 0.5,
     );
 
-    ctx.lineTo(this.sight.width, this.sight.height);
-    ctx.lineTo(0, this.sight.height);
+    this.ctx.lineTo(this.sight.width, this.sight.height);
+    this.ctx.lineTo(0, this.sight.height);
 
-    ctx.fillStyle = 'rgba(0, 0, 0)';
-    ctx?.fill();
-  }
-
-  render() {
-    this.target.appendChild(this.sight);
+    this.ctx.fillStyle = 'rgba(0, 0, 0)';
+    this.ctx?.fill();
+    // this.target.appendChild(this.sight);
   }
 }
 
