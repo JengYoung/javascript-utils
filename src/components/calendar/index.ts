4674a89cbfd3fb7ed601a8286a4a3ea9bc789112
JengYoung <young_pallete@naver.com>
2022-10-06 18:34:01 +0900

    :sparkles: DateCell을 현재 날짜 기준으로 만들도록 구현
    다만, 년/월에 따라 동적으로 렌더링되지는 않음.

--- src/components/calendar/index.ts
+++ src/components/calendar/index.ts
@@ -1,4 +1,4 @@
-import DateCell from './dateCell';
+import DateCell from './DateCell';
 
 class Calendar {
   target: Element;
@@ -6,6 +6,14 @@ class Calendar {
   container: HTMLElement;
 
   inner: HTMLElement;
+
+  nowDate: Date;
+
+  year: number;
+
+  month: number;
+
+  date: number;
 
   constructor(target: Element) {
     this.target = target;
@@ -15,19 +23,57 @@ class Calendar {
 
     this.inner = document.createElement('div');
     this.inner.classList.add('calendar__inner');
+
+    this.nowDate = new Date();
+
+    this.year = this.nowDate.getFullYear();
+    this.month = this.nowDate.getMonth();
+    this.date = this.nowDate.getDate();
   }
 
-  addCell() {
-    for (let i = 0; i < 32; i += 1) {
-      const dateCell = new DateCell(this.inner);
-      dateCell.setDate(i);
+  #getLastDate(year: number, month: number, date: number) {
+    let dateCount = 1;
+    let now = new Date(year, month, date);
 
-      dateCell.render();
+    while (now.getMonth() === this.month) {
+      dateCount += 1;
+      now = new Date(now.setDate(dateCount));
+    }
+
+    return dateCount - 1;
+  }
+
+  #makeCell(date?: number) {
+    const dateCell = new DateCell(this.inner);
+    dateCell.render();
+    if (typeof date === 'number') {
+      dateCell.setDate(date);
+    }
+  }
+
+  makeCalendar() {
+    const lastDate = this.#getLastDate(this.year, this.month, this.date);
+
+    // NOTE: 윌월화수목금토
+    const firstDayIndex = new Date(this.year, this.month).getDay();
+
+    const afterLastDateCount = 7 - ((firstDayIndex + lastDate) % 7);
+
+    for (let i = 0; i < firstDayIndex; i += 1) {
+      this.#makeCell();
+    }
+
+    for (let i = 1; i <= lastDate; i += 1) {
+      this.#makeCell(i);
+    }
+
+    for (let i = 0; i < afterLastDateCount; i += 1) {
+      this.#makeCell();
     }
   }
 
   render() {
-    this.addCell();
+    this.makeCalendar();
     this.container.appendChild(this.inner);
     this.target.appendChild(this.container);
   }
