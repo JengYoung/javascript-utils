7912a3436fe7aef079ed36c7ec9da03e8586576b
JengYoung <young_pallete@naver.com>
2022-10-20 16:03:22 +0900

    :truck: 새로운 방법으로 구현하기 위해 디렉토리 구조 변경 및 초기화
    아예 path를 그려내는 방식으로 구현해보고자 한다.

--- src/components/canvas/Metaball/main.ts
+++ src/components/canvas/Metaball/main.ts
@@ -3,8 +3,6 @@
  */
 
 import Canvas from '..';
-import {Metaballs} from './MetaBalls';
-import {Points} from './Points';
 
 export class App {
   target: Element;
@@ -15,10 +13,6 @@ export class App {
 
   height: number;
 
-  metaballs: Metaballs;
-
-  points: Points;
-
   constructor(target: Element) {
     this.target = target;
 
@@ -26,43 +20,10 @@ export class App {
     this.height = window.innerHeight;
 
     this.canvas = new Canvas(this.target, this.width, this.height);
-
-    this.metaballs = new Metaballs({ctx: this.canvas.ctx, state: {ballCnt: 2}});
-
-    this.points = new Points({
-      width: this.width,
-      height: this.height,
-      gapWeight: 2,
-    });
   }
 
   render() {
     this.canvas.render();
-    this.metaballs.render();
-
-    this.animate();
-  }
-
-  animate() {
-    this.canvas.ctx.clearRect(0, 0, this.width, this.height);
-    requestAnimationFrame(() => this.animate());
-
-    this.metaballs.move();
-
-    this.points.points.forEach(({x: x2, y: y2}, i) => {
-      if (i % 2) return;
-      let total = 0;
-      this.metaballs.arr.forEach(metaball => {
-        const {x: x1, y: y1, r} = metaball.state;
-
-        total += r / ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5;
-      });
-
-      if (total >= 1.2) {
-        this.canvas.ctx.fillStyle = 'orange';
-        this.canvas.ctx.fillRect(x2, y2, 2, 2);
-      }
-    });
   }
 }
 
