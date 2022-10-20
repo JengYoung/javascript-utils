f16bb38ba3d2dc263c374a51242e9790448ea4db
JengYoung <young_pallete@naver.com>
2022-10-20 16:48:38 +0900

    :sparkles: 그라데이션 추가

--- src/components/canvas/Metaball/main.ts
+++ src/components/canvas/Metaball/main.ts
@@ -1,3 +1,4 @@
+import './main.module.scss';
 /**
  * 메타볼을 실행하는 클래스입니다.
  */
@@ -7,14 +8,16 @@ import {Metaball} from './Metaball';
 
 interface AppInterface {
   target: Element;
-
   canvas: Canvas;
 
   width: number;
-
   height: number;
 
-  metaball: Metaball;
+  metaball1: Metaball;
+  metaball2: Metaball;
+
+  bgGradients: [string, string];
+  gradients: [string, string];
 }
 
 export class App implements AppInterface {
@@ -26,7 +29,13 @@ export class App implements AppInterface {
 
   height: number;
 
-  metaball: Metaball;
+  metaball1: Metaball;
+
+  metaball2: Metaball;
+
+  bgGradients: [string, string];
+
+  gradients: [string, string];
 
   constructor(target: Element) {
     this.target = target;
@@ -36,20 +45,46 @@ export class App implements AppInterface {
 
     this.canvas = new Canvas(this.target, this.width, this.height);
 
-    this.metaball = new Metaball({
-      x: 100,
-      y: 100,
-      r: 50,
+    this.metaball1 = new Metaball({
+      x: 200,
+      y: 200,
+      r: 150,
     });
+
+    this.metaball2 = new Metaball({
+      x: 500,
+      y: 500,
+      r: 200,
+    });
+
+    this.bgGradients = ['#85ffff', '#c2a4f9'];
+    this.gradients = ['#00ffff', '#752bed'];
   }
 
   get ctx() {
     return this.canvas.ctx;
   }
 
+  gradient(gradients: [string, string]) {
+    const result = this.ctx.createLinearGradient(0, 0, 0, this.height);
+
+    gradients.forEach((gradient, idx) => {
+      result.addColorStop(idx, gradient);
+    });
+
+    return result;
+  }
+
   render() {
+    const bgGradiation = this.gradient(this.bgGradients);
+    this.ctx.fillStyle = bgGradiation;
+    this.ctx.fillRect(0, 0, this.width, this.height);
+
+    const metaballGradiation = this.gradient(this.gradients);
+    this.ctx.fillStyle = metaballGradiation;
     this.canvas.render();
-    this.metaball.render(this.ctx);
+    this.metaball1.render(this.ctx);
+    this.metaball2.render(this.ctx);
     this.ctx.fill();
   }
 }
