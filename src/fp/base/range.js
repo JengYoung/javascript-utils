66d43f0d0afa3bf939ac9dc2841aa18ec54a32cb
JengYoung <young_pallete@naver.com>
2023-10-16 13:46:24 +0900

    :sparkles: Range 함수 구현

---
+++ src/fp/base/range.js
@@ -0,0 +1,14 @@
+import {reduce} from './index.mjs';
+
+export const range = (to, from = 0, res = []) => {
+  if (to <= from) {
+    return res;
+  }
+
+  return range(to, from + 1, [...res, from]);
+};
+
+const add = (a, b) => a + b;
+
+const list = range(4);
+console.log(reduce(add, list));
